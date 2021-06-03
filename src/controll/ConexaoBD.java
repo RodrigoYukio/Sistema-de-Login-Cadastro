4fce7b319e1ba28fc963d02c2f39ccba60b38d7a
Rodrigo <rodrigo_ys@hotmail.com>
2021-06-03 16:41:58 -0300

    Initial commit

---
+++ src/controll/ConexaoBD.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controll;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author rodri
+ */
+public class ConexaoBD {
+    public static Connection getConnection(){
+        
+        Connection con = null;
+        
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            con = DriverManager.getConnection("jdbc:mysql://localhost/login_cadastro", "root","");
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return con;
+    }
+    
+}
+    
